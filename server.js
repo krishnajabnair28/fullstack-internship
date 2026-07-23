59fc629ce3ba013884c931b4cb4f88e486265160
Krishnaja B Nair <krishnajabnair28@gmail.com>
2026-07-23 22:34:00 +0530

    Completed Day 5 - Express Server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to my Express Server!");
+});
+app.post("/blog", (req, res) => {
+    res.send("Blog added successfully!");
 });
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
