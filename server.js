c1a203d6c89db7eb7a4220e07c183e30dd7b7eb2
Krishnaja B Nair <krishnajabnair28@gmail.com>
2026-07-24 21:58:50 +0530

    Completed Day 6 - Add Blog API

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const PORT = 3000;
+const blogs = [];
 app.get("/", (req, res) => {
     res.send("Welcome to my Express Server!");
 });
 app.post("/blog", (req, res) => {
-    res.send("Blog added successfully!");
+    const { title, content } = req.body;
+    const newBlog = { id: blogs.length + 1, title, content };
+    blogs.push(newBlog);
+    res.status(201).send("Blog added successfully!");
+});
+app.get("/blogs", (req, res) => {
+    res.json(blogs);
 });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
